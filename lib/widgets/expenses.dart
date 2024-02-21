dd7bbdc081579079ab1dc4050917ab5db06f588c
Veng-Eang <oengvengeang@gmail.com>
2024-02-21 10:43:16 -0500

    add constructor

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -5,6 +5,8 @@ import 'package:expanse_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
   @override
   State<Expenses> createState() {
     return _ExpensesState();
